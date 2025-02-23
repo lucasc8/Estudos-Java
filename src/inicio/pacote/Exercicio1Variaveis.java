3311a7ff1aa882507e3a8620e56c1e0091c6acaf
Lucas <l.gabrielalb@gmail.com>
2025-02-23 07:22:31 -0300

    Arquivos Iniciais

---
+++ src/inicio/pacote/Exercicio1Variaveis.java
@@ -0,0 +1,12 @@
+package inicio.pacote;
+
+public class Exercicio1Variaveis {
+    // exericio: eu <nome>, morando no endereco <endereco>, confirmo que recebi o salario de <valor>, na data <data>
+    public static void main(String[] args) {
+        String nome = "Lucas";
+        String endereco = "Rua alvares de cabral num 10";
+        int salario = 1000;
+        String data = "03/10/2023";
+        System.out.println("Eu, " + nome + ", confirmo que recebi o salário de " + salario + ", no endereco "+ endereco + ", na data de " + data + ".");
+    }
+}
---
+++ src/inicio/pacote/TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package inicio.pacote;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // int double float char byte short long boolean
+        int idadeDoPai = (int) 4444444444444L;
+        System.out.println("A idade é: " + idadeDoPai);
+        String nome = "a a a a lepo lepo";
+    }
+}
