f5b78defde0ad0b3e65205e2234d5fd79325ce44
lucasc8 <l.gabrielalb@gmail.com>
2025-02-25 22:27:45 -0300

    feat Bloco de Inicialização

---
+++ src/POO/BlocosdeInicializacao/dominio/Desenho.java
@@ -0,0 +1,7 @@
+package POO.BlocosdeInicializacao.dominio;
+
+public class Desenho {
+    {
+        System.out.println("código no bloco de inicialização");
+    }
+}
---
+++ src/POO/BlocosdeInicializacao/teste/DesenhoTeste.java
@@ -0,0 +1,12 @@
+package POO.BlocosdeInicializacao.teste;
+
+import POO.BlocosdeInicializacao.dominio.Desenho;
+
+public class DesenhoTeste {
+    public static void main(String[] args) {
+        Desenho desenho1 = new Desenho();
+        Desenho desenho2 = new Desenho();
+        Desenho desenho3 = new Desenho();
+        Desenho desenho4 = new Desenho();
+    }
+}
